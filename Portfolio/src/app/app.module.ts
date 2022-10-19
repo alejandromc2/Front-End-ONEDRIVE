8d5f716ae62b9e7c3cc4bd6eaca2bb1c1ff7115a
alejandromc2 <alejandromartin.1592@gmail.com>
2022-10-19 14:35:56 -0300

    divided in angular components

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { SoftwareComponent } from './software/software.component';
+import { ExperienciaLaboralComponent } from './experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { DatosDeContactoComponent } from './datos-de-contacto/datos-de-contacto.component';
+import { MandameUnMensajeComponent } from './mandame-un-mensaje/mandame-un-mensaje.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SobreMiComponent,
+    SoftwareComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    DatosDeContactoComponent,
+    MandameUnMensajeComponent
   ],
   imports: [
     BrowserModule
