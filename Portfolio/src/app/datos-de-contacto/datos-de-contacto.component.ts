f7093a44f7c9048cab4e8f7f021fc676a495eb87
alejandromc2 <alejandromartin.1592@gmail.com>
2022-10-19 17:08:38 -0300

    respective angular components now working

---
+++ Portfolio/src/app/datos-de-contacto/datos-de-contacto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-de-contacto',
+  templateUrl: './datos-de-contacto.component.html',
+  styleUrls: ['./datos-de-contacto.component.css']
+})
+export class DatosDeContactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Portfolio/src/app/mandame-un-mensaje/mandame-un-mensaje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mandame-un-mensaje',
+  templateUrl: './mandame-un-mensaje.component.html',
+  styleUrls: ['./mandame-un-mensaje.component.css']
+})
+export class MandameUnMensajeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
